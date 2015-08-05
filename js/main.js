030623daabf10bd589d1791a534af47c093a102b
Sean Joyal <sean.joyal@gmail.com>
2015-08-05 14:04:21 -0400

    began looking at scrollspy, added transparent background to a:focus links in navbar

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 (function() {
   'use strict';
 
-  $('.blog-post')
-    .on('click', 'h2', function(){
-      $('#show').toggleClass('show');
+  $('.home-button')
+    .on('click', function(){
+      event.preventDefault();
+      $('html,body').animate({scrollTop: 0}, 500);
+    });
+
+  $('.main-buttons')
+    .on('click', 'a[href^="#"]', function(){
+      event.preventDefault();
+      var scrollTo = $($(this).attr('href')).offset().top;
+      $('html,body').animate({scrollTop: scrollTo}, 500);
+    });
+
+  $('.circle')
+    .on('click', function(){
+      event.preventDefault();
+      var scrollPort = $('#portfolio').offset().top;
+      $('html,body').animate({scrollTop: scrollPort}, 500);
     });
 
   $('#older-portfolio')
@@ -21,24 +36,9 @@
       $('#iron-pong-modal').modal('toggle');
     });
 
-  $('.home-button')
-    .on('click', function(){
-      event.preventDefault();
-      $('html,body').animate({scrollTop: 0}, 500);
-    });
-
-  $('.main-buttons')
-    .on('click', 'a[href^="#"]', function(){
-      event.preventDefault();
-      var scrollTo = $($(this).attr('href')).offset().top;
-      $('html,body').animate({scrollTop: scrollTo}, 500);
-    })
-
-  $('.circle')
-    .on('click', function(){
-      event.preventDefault();
-      var scrollPort = $('#portfolio').offset().top;
-      $('html,body').animate({scrollTop: scrollPort}, 500);
+  $('.blog-post')
+    .on('click', 'h2', function(){
+      $('#show').toggleClass('show');
     });
 
 })();
